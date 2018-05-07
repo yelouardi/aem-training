aa0881e8cf8c15ff5e12219803a36f8a8a4f59b4
EL OUARDI YASSINE <ing.elouardi@gmail.com>
2018-05-07 13:04:24 +0200

    Update IdenditeModel.java

--- bundle/src/main/java/com/aem/training/model/IdenditeModel.java
+++ bundle/src/main/java/com/aem/training/model/IdenditeModel.java
@@ -12,26 +12,14 @@ import org.slf4j.LoggerFactory;
 @Model(adaptables=Resource.class)
 public class IdenditeModel {
 	
-	
-	
-
-	
-	
-	private static final Logger LOGGER = LoggerFactory.getLogger(IdenditeModel.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(IdenditeModel.class);
 
     @Self
     private Resource identite;
     
-    
-    
     public String  getMessageDisplay (){
-
-    	
     	ValueMap values = identite.adaptTo(ValueMap.class);
-    	LOGGER.error("{} , ------------------ IDENTITE -------------------------",					values.get("identite", String.class));
-    	LOGGER.error("{} , ------------------ NOM -------------------------",					values.get("nom", String.class));
-    	LOGGER.error("{} , ------------------ PRENOM -------------------------",					values.get("preNom", String.class));
-		return String.format("BienVennue : %s %s %s dans la formation AEM", values.get("identite", String.class), values.get("nom", String.class),
+    	return String.format("BienVennue : %s %s %s dans la formation AEM", values.get("identite", String.class), values.get("nom", String.class),
 				values.get("preNom", String.class));
 	}
     	
